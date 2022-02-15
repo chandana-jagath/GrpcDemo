d85154637ca466133979406efb4b5085818009f1
Chandana Jagath <pushpakumara.chandana@gmail.com>
2022-02-15 09:50:15 +0530

    Add project files.

---
+++ GrpcService/Services/CustomerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomeModel> GetCustomerInfo(CustomerLockupModel request, ServerCallContext context)
+        {
+            CustomeModel customeModel = new CustomeModel();
+
+            if (request.UserId == 1)
+            {
+                customeModel.FirstName = "Nik";
+                customeModel.LastName = "Jon";
+                customeModel.Age = 35;
+            }
+            if (request.UserId == 2)
+            {
+                customeModel.FirstName = "Jack";
+                customeModel.LastName = "Marc";
+                customeModel.Age = 34;
+            }
+
+            return Task.FromResult(customeModel);
+        }
+  
+   }
+}
---
+++ gRPCClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcService;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new CustomerLockupModel { UserId = 5 };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Customer.CustomerClient(channel);
+            var reply = await client.GetCustomerInfoAsync(input);
+            Console.WriteLine($"{reply.FirstName} {reply.LastName} {reply.Age}");
+            Console.ReadLine();
+        }
+    }
+}
